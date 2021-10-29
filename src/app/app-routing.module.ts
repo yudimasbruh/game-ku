9b4ddce907d1c9c20b2fc4e65d756930913bc4fd
yudimasbruh <yudimuttaqin33@gmail.com>
2021-10-29 16:58:19 +0700

    projek kedua adalah gameku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile',component:ProfileComponent},
+  {path:'tournament',component:TournamentComponent},
+  {path:'leaderboard',component:LeaderboardComponent},
+  {path:'home',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
